221bf32e81677380bc7925ed585e6da463921531
darkness8129 <yaroslaw460@gmail.com>
2020-07-15 19:54:46 +0300

    readme added

--- notes-app/src/components/ColorSwitcher.js
+++ notes-app/src/components/ColorSwitcher.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import '../style/color-switcher.css';
 
 export default class ColorSwitcher extends Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.setActive = this.setActive.bind(this);
-	}
+        this.setActive = this.setActive.bind(this);
+    }
 
-	//func that transfer color to change color of note and set active switcher
-	setActive() {
-		this.props.setActive(this.props.color);
-		this.props.onColorChange(this.props.color);
-	}
+    //func that transfer color to change color of note and set active switcher
+    setActive() {
+        this.props.setActive(this.props.color);
+        this.props.onColorChange(this.props.color);
+    }
 
-	render() {
-		let styleSwitcher = { backgroundColor: this.props.color };
-		styleSwitcher.color = this.props.isActive === false ? "transparent" : "black";
-		return <div className="color-switcher" style={styleSwitcher} onClick={this.setActive}> ✔ </div>;
-	}
+    render() {
+        let styleSwitcher = { backgroundColor: this.props.color };
+        styleSwitcher.color = this.props.isActive === false ? "transparent" : "black";
+        return <div className="color-switcher" style={styleSwitcher} onClick={this.setActive}> ✔ </div>;
+    }
 }
